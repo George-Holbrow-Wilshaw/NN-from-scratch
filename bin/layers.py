ab27449ac27c856390e1236609cc6b41891b28c1
George Holbrow-Wilshaw <georgewilshaw1996@gmail.com>
2020-12-27 23:18:15 +0000

    Adding sequential class

--- bin/layers.py
+++ bin/layers.py
@@ -1,15 +1,29 @@
-class NeuralNetwork:
+class SequentialNetwork:
 
-    def __init__(self, layers, learning_rate, epochs, batch_size):
-        pass
+    def __init__(self, layers):
+        self.layers = layers
 
-    def compile(self, optimiser, loss_function):
-        pass
+    def compile(self, optimiser, loss_function, metrics):
+        net = NeuralNetwork(layers = self.layers, 
+                            optimiser = optimiser, 
+                            loss_function = loss_function, 
+                            metrics = metrics)
+        return(net)
 
     def summary(self):
+        for l in self.layers:
+            print('%s Input shape = %s  Output shape = %s' %(l.name, l.input_shape, l.output_shape))
+
+
+class NeuralNetwork:
+
+    def __init__(self, layers, optimiser, loss_function, metrics):
         pass
 
     def back_propogate(self):
+        pass
+
+    def forward_propogate(self):
         pass
 
     def calc_loss(self):
@@ -17,33 +31,38 @@ class NeuralNetwork:
 
     def produce_metrics(self):
         pass
-    
-    def fit(self):
+
+    def fit(self, training_data, validation_data, metrics):
         pass
 
     def predict(self):
         pass
     
 
-class NetworkLayer(NeuralNetwork):
-
-    def __init__(self):
-        pass
 
 
-class ConvolutionLayer(NetworkLayer):
+class ConvolutionLayer:
 
-    def __init__(self, filters, kernel_size, input_shape, activation_function):
-        pass
+    def __init__(self, n_filters, kernel_size, input_shape, activation_function):
+        self.name = 'ConvolutionLayer'
+        self.input_shape = input_shape
+        self.output_shape = 'tbd'
+        self.kernel_size = kernel_size
+        self.n_filters = n_filters
+        self.activation_function = activation_function 
 
     def build_layer(self):
         pass
 
 
-class FullyConnected(NetworkLayer):
+class FullyConnectedLayer:
 
-    def __init__(self, units, input_shape, activation_function):
-        pass
+    def __init__(self, n_nodes, input_shape, activation_function):
+        self.name = 'FullyConnectedLayer'
+        self.input_shape = input_shape
+        self.output_shape = 'tbd'
+        self.n_units = n_nodes
+        self.activation_function = activation_function
 
     def build_layer(self):
         pass
